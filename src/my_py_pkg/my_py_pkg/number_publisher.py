a51e9d4f1ca289b9ed0e48bf6170abeee749abf6
Joao Victor <jvictor3651@gmail.com>
2026-04-29 18:38:50 -0300

    módulo de laucnh files

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 from example_interfaces.msg import Int64
 
 
@@ -11,6 +12,8 @@ class NumberPublisherNode(Node):
         self.declare_parameter("timer_period", 1.0)
         self.number_ = self.get_parameter("number").value
         self.timer_period_ = self.get_parameter("timer_period").value
+        self.add_post_set_parameters_callback(self.parameters_callback)
+
         self.publisher = self.create_publisher(Int64 , "number", 10)
         self.timer_ = self.create_timer(self.timer_period_, self.publish_number)
         self.get_logger().info("The number is being published!")
@@ -20,6 +23,11 @@ class NumberPublisherNode(Node):
         msg.data = self.number_
         self.publisher.publish(msg)
 
+    def parameters_callback(self, params: list[Parameter]):
+        for param in params:
+            if param.name == "number":
+                self.number_ = param.value
+                
 
 
 
